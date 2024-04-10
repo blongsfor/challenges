f352efe5d1069d259d862f3c90bc4491af474aa0
Björn <bjoern.n@mailbox.org>
2024-04-10 13:45:30 +0200

    Add calculate js file and the calculations

---
+++ javascript/calculate-circle-area-and-circumference/calculate-area-circumference.js
@@ -0,0 +1,7 @@
+let radius = 5;
+let circleArea = Math.PI * Math.pow(radius, 2);
+let circumference = 2 * Math.PI * radius;
+
+console.log("For a circle with a radius of 5 units:");
+console.log("The area is: ", circleArea);
+console.log("The circumference is: ", circumference);
