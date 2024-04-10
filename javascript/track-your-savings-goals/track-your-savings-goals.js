d2551978c5637b478a7ed501be1875a2d1b052e5
Björn <bjoern.n@mailbox.org>
2024-04-10 12:14:35 +0200

    add js file and calculations

---
+++ javascript/track-your-savings-goals/track-your-savings-goals.js
@@ -0,0 +1,7 @@
+let startingBalance = 1000;
+let monthlyContribution = 50;
+let months = 30;
+
+let totalSavings = startingBalance + monthlyContribution * months;
+
+console.log("After 30 months the total savings are: ", totalSavings);
