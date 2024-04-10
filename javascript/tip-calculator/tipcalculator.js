f9918e9de8a7affa8c87db5f68330f4df8291e4e
Björn <bjoern.n@mailbox.org>
2024-04-10 11:58:40 +0200

    add tipcalculator.js with caculation

---
+++ javascript/tip-calculator/tipcalculator.js
@@ -0,0 +1,7 @@
+let mealCost = 50;
+let tipPercentage = 10;
+
+let tipAmount = (mealCost * tipPercentage) / 100;
+console.log("The tipamount is: ", tipAmount);
+let totalCost = mealCost + tipAmount;
+console.log("The total cost is: ", totalCost);
