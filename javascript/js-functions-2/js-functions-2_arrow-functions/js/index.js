61bb1bd0f566bccc076cb05f0b8f4b771df0080c
Björn <bjoern.n@mailbox.org>
2024-04-15 22:03:57 +0200

    arrow function to classic function

--- javascript/js-functions-2/js-functions-2_arrow-functions/js/index.js
+++ javascript/js-functions-2/js-functions-2_arrow-functions/js/index.js
@@ -17,19 +17,19 @@ const getCurrentHour = () => {
     : "${currentHour -12}pm";
 };
 
-function getVectorLength(x, y, z) {
-  return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
-}
+const getVectorLength = (x, y, z) => {
+  (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+};
 
-function cleanInput(string) {
-  return string.toLowerCase().trim();
-}
+const cleanInput = (string) => {
+  string.toLowerCase().trim();
+};
 
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
-const isOddAndSmall = (number) => {
+function isOddAndSmall(number) {
   if (number > 10) {
     return false;
   }
@@ -39,8 +39,12 @@ const isOddAndSmall = (number) => {
   }
 
   return true;
-};
+}
 
-const add3 = (a, b, c) => a + b + c;
+function add3(a, b, c) {
+  return a + b + c;
+}
 
-const repeat10 = (string) => string.repeat(10);
+function repeat10(string) {
+  return string.repeat(10);
+}
