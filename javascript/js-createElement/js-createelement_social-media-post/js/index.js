c1ebfdcc2a541b9d276135d70bf90aead2c34d54
Björn <bjoern.n@mailbox.org>
2024-04-19 19:55:39 +0200

    change

--- javascript/js-createElement/js-createelement_social-media-post/js/index.js
+++ javascript/js-createElement/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,26 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const newSocialMediaPost = document.createElement("section");
+newSocialMediaPost.classList.add("post");
+
+const newSocialmediaPost = document.createElement("p");
+newSocialmediaPost.textContent = "Please like, share and subscribe :)";
+newSocialmediaPost.classList.add("post__content");
+
+const newFooter = document.createElement("footer");
+newFooter.classList.add("post__footer");
+
+const newSpan = document.createElement("span");
+newSpan.textContent = "@username";
+newSpan.classList.add("post__username");
+
+const newButton = document.createElement("button");
+newButton.textContent = "♥ Like";
+newButton.classList.add("post__button");
+
+newFooter.append(newSpan, newButton);
+newSection.append(newSocialmediaPost, newFooter);
+
+document.body.append(newSection);
