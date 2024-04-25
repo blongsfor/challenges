e4e9cbb5b5e4cb228d8a6389e7c7c44521504b91
Björn <bjoern.n@mailbox.org>
2024-04-25 13:59:41 +0200

    doing creating elements instead of innerHTML

--- javascript/js-createElement/js-createelement_product-teaser/js/index.js
+++ javascript/js-createElement/js-createelement_product-teaser/js/index.js
@@ -9,3 +9,49 @@ const category3 = "Plankton Diet";
 const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+const newSection = document.createElement("article");
+newSection.classList.add("product");
+
+const productBodyDiv = document.createElement("div");
+productBodydiv.classList.add("product__body");
+
+const productTextContainerDiv = document.createElement("div");
+productTextContainerDiv.classList.add("product__text-container");
+
+const productNameHeadline = document.createElement("h2");
+productNameHeadline.classList.add("product__name");
+productNameHeadline.textContent = "Clownfish";
+
+const productCategoriesUl = document.createElement("ul");
+productCategoriesUl.classList.add("product__categories");
+
+const productCategoryLi = document.createElement("li");
+productCategoryLi.classList.add("product__category");
+productCategoryLi.textContent = "warmes Wasser";
+
+const productDescriptionParagraph = document.createElement("p");
+productDescriptionParagraph.classList.add("product__description");
+productDescriptionParagraph.textContent = "alles für die Fische";
+
+const productImageContainer = document.createElement("div");
+productImageContainer.classList.add("product__image-container");
+
+const productImage = document.createElement("img");
+productImage.classList.add("product__image");
+productImage.src =
+  "https://unsplash.com/de/fotos/orange-und-weisse-clownfische-x9yfTxHpj5w";
+
+const footer = document.createElement("footer");
+footer.classList.add("product__footer");
+
+const footerSpan = document.createElement("span");
+footerSpan.classList.add("product__price");
+footerSpan.textContent = "14€";
+
+const button = document.createElement("button");
+button.classList.add("product__buy-button");
+
+document.body.append(newSection);
+newSection.append(productBodyDiv);
+newSection.append();
