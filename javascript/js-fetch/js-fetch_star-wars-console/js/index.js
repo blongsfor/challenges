d0915e1aea2e79027d327eec209aa2baba0e9fce
Björn <bjoern.n@mailbox.org>
2024-04-26 12:09:32 +0200

    doing challenge

--- javascript/js-fetch/js-fetch_star-wars-console/js/index.js
+++ javascript/js-fetch/js-fetch_star-wars-console/js/index.js
@@ -1,7 +1,17 @@
 console.clear();
 
-const url = "https://swapi.dev/api/people";
+const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
-
-fetchData();
+try {
+  async function fetchData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    console.log("R2D2", data.results[2]);
+    console.log("R2D2 eyecolor: ", data.results[2].eye_color);
+    return data;
+  }
+  fetchData();
+} catch (error) {
+  ("Something went wrong");
+}
