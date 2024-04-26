3b11c53650bd73c064a2f5eff26499e4e1e5f439
Björn <bjoern.n@mailbox.org>
2024-04-26 13:39:44 +0200

    fetch data from api

--- javascript/js-fetch/js-fetch_star-wars/js/index.js
+++ javascript/js-fetch/js-fetch_star-wars/js/index.js
@@ -41,6 +41,16 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.py4e.com/api/people");
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
+
+results.forEach((result) => {
+  Card(result);
+  renderElement(result);
+});
+
+fetch(); // ?
