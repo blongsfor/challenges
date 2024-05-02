32520c2c54ef61fcc46cd97d00ded205dc34794f
Björn <bjoern.n@mailbox.org>
2024-05-02 14:54:50 +0200

    abc

--- javascript/js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ javascript/js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -16,3 +16,29 @@ For further information check MDN:
 - about CSS declarations with "style": https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
 
 */
+/*
+const box = document.querySelector('[data-js="box"]');
+const colorChange = document.querySelector('[data-js="input-color"]');
+
+colorChange.addEventListener("input", () => {
+  box.style.background = `hsl(${colorInput.value}, 70%, 60%)`;
+});
+*/
+
+const box = document.querySelector('[data-js="box"]');
+const colorInput = document.querySelector('[data-js="input-color"]');
+const radiusInput = document.querySelector('[data-js="input-radius"]');
+const rotationInput = document.querySelector('[data-js="input-rotation"]');
+
+colorInput.addEventListener("input", () => {
+  box.style.background = `hsl(${colorInput.value}, 70%, 60%)`;
+});
+
+radiusInput.addEventListener("input", () => {
+  box.style.borderRadius = `${radiusInput.value}%`;
+});
+
+rotationInput.addEventListener("input", () => {
+  box.style.transform = `rotateZ(${rotationInput.value}deg)`;
+  console.log(box.style.transform);
+});
