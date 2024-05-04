d419cce406065875d379c22b83cda9b4f82e5767
Björn <bjoern.n@mailbox.org>
2024-05-04 17:39:10 +0200

    doing attribute challenge

--- react/react-basics/react-basics_attributes/src/App.jsx
+++ react/react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return Article();
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Futurama is the best</h2>
+      <label id="label" htmlFor="empty">
+        Futurama everywhere
+      </label>
+      <input id="input" type="text"></input>
+      <a
+        className="article__link"
+        href="https://de.wikipedia.org/wiki/Futurama"
+      >
+        get to know more about Futurama
+      </a>
+    </article>
+  );
 }
