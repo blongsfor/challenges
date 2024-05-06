50e93bd3e848e61980aa6d8f4cac149ca96dcc52
Björn <bjoern.n@mailbox.org>
2024-05-06 11:22:29 +0200

    sum challenge

--- react/react-props/react-props_sum/src/App.jsx
+++ react/react-props/react-props_sum/src/App.jsx
@@ -1,5 +1,13 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA={8} valueB={2} />;
+}
+
+function Sum({ valueA, valueB }) {
+  return (
+    <h1>
+      {valueA} + {valueB} = {valueA + valueB}
+    </h1>
+  );
 }
