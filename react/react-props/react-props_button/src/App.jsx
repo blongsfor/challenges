8043ca49846e0dccfda4dbcb81246bb1b56ce8a6
Björn <bjoern.n@mailbox.org>
2024-05-06 13:34:57 +0200

    add button

--- react/react-props/react-props_button/src/App.jsx
+++ react/react-props/react-props_button/src/App.jsx
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Button text="test" color={"#FF0000"} disabled={false} />;
+}
+
+function Button({ color, disabled, text }) {
+  return (
+    <button
+      style={{
+        height: "100px",
+        width: "200px",
+        color: color,
+      }}
+    >
+      {text} {disabled}
+    </button>
+  );
 }
