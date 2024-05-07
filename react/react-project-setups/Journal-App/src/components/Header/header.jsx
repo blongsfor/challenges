738a4330924c98e00849cc999ea7d81f8a213741
Björn <bjoern.n@mailbox.org>
2024-05-07 14:00:49 +0200

    add tabs.jsx

--- react/react-project-setups/Journal-App/src/components/Header/header.jsx
+++ react/react-project-setups/Journal-App/src/components/Header/header.jsx
@@ -1,7 +1,9 @@
 export function Header() {
   return (
-    <header className="header">
-      <h1>Journal</h1>
-    </header>
+    <>
+      <header className="header">
+        <h1>Journal</h1>
+      </header>
+    </>
   );
 }
---
+++ react/react-project-setups/Journal-App/src/components/Tabs/tabs.jsx
@@ -0,0 +1,9 @@
+export function Tabs() {
+  return (
+    <section className="tabs-section">
+      <div className="tabs__tab-one">
+        <tab className="tab-one"></tab>
+      </div>
+    </section>
+  );
+}
