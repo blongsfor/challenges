a49e435a5d500d29097afb29aff0d5dd183799b8
Björn <bjoern.n@mailbox.org>
2024-05-09 22:19:47 +0200

    add bookmark sign

--- react/react-project-setups/Journal-App/src/components/entry.jsx
+++ react/react-project-setups/Journal-App/src/components/entry.jsx
@@ -1,7 +1,11 @@
+import star from "../assets/star.svg";
+import unstar from "../assets/unstar.svg";
+
 export function Entry({ date, title, text }) {
   return (
     <>
       <div className="entry">
+        <img className="bookmark-image" src={star} alt="bookmark" />
         <date>{date}</date>
         <h3 className="entry-titel">{title}</h3>
         <p className="entry-text">{text}</p>
