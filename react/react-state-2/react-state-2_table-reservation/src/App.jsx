fb2d3f6babbe44a4630bbc3e09024d2a52133619
Björn <bjoern.n@mailbox.org>
2024-05-10 16:20:22 +0200

    add counters

--- react/react-state-2/react-state-2_table-reservation/src/App.jsx
+++ react/react-state-2/react-state-2_table-reservation/src/App.jsx
@@ -1,11 +1,21 @@
 import Counter from "./components/Counter";
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  const handlePlus = () => {
+    setPeople(people + 1);
+  };
+  const handleMinus = () => {
+    setPeople(people - 1);
+  };
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
+      <Counter onhandlePlus={handlePlus} onhandleMinus={handleMinus} />
       <p>You are going to reserve a table for 2 people.</p>
     </div>
   );
