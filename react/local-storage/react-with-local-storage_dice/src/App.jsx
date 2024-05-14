8732c4caa0ce0945115df45018a412d9af1ce2dd
Björn <bjoern.n@mailbox.org>
2024-05-14 12:50:11 +0200

    add some solutions to task

--- react/local-storage/react-with-local-storage_dice/src/App.jsx
+++ react/local-storage/react-with-local-storage_dice/src/App.jsx
@@ -1,8 +1,13 @@
 import { D6Button } from "./components/D6Button";
 import History from "./components/History";
 import "./App.css";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("rolls", {
+    defaultValue: [],
+  });
+
   return (
     <div className="app">
       <main className="app__main">
