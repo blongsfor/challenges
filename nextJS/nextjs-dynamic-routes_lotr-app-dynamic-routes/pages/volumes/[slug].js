5dfff80956827336389786f15c41ee3ac11594f8
Bjoern <bjoern.n@mailbox.org>
2024-05-20 19:04:06 +0200

    move files to hide folder and refactor by creating [slug]

---
+++ nextJS/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/[slug].js
@@ -0,0 +1,47 @@
+import { useRouter } from "next/router";
+import { Link } from "next/link";
+import { Image } from "next/dist/server/image-optimizer";
+
+export default function VolumeDetail() {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const thisVolume = volumes.find((volume) => volume.slug === slug);
+
+  const { title, description, cover, books } = thisVolume;
+
+  return (
+    <>
+      <Link href="/volumes">← All Volumes</Link>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <ul>
+        {books.map(({ ordinal, title }) => (
+          <li key={title}>
+            {ordinal}: <strong>{title}</strong>
+          </li>
+        ))}
+      </ul>
+      <Image
+        src={cover}
+        alt={`Cover image of ${title}`}
+        width={140}
+        height={230}
+      />
+      {previousVolume ? (
+        <div>
+          <Link href={`/volumes/${previousVolume.slug}`}>
+            ← Previous Volume: {previousVolume.title}
+          </Link>
+        </div>
+      ) : null}
+      {nextVolume ? (
+        <div>
+          <Link href={`/volumes/${nextVolume.slug}`}>
+            Next Volume: {nextVolume.title} →
+          </Link>
+        </div>
+      ) : null}
+    </>
+  );
+}
