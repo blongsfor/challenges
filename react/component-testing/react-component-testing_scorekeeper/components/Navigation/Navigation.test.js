664e58fca3aa7b01432767491f4796de577c82ae
Björn <bjoern.n@mailbox.org>
2024-05-21 11:23:35 +0200

    add testing for navigation

--- react/component-testing/react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
+++ react/component-testing/react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Navigation from ".";
+import "@testing-library/jest-dom/extend-expect";
 
 jest.mock("next/router", () => ({
   useRouter() {
@@ -7,4 +8,10 @@ jest.mock("next/router", () => ({
   },
 }));
 
-test("renders with two links 'Play' and 'History'", () => {});
+test("renders with two links 'Play' and 'History'", () => {
+  render(<Navigation />);
+  const playLink = screen.getByRole("link", { name: /play/i }); // /i damit case-sensitivity egal ist
+  const historyLink = screen.getByRole("link", { name: /history/i });
+  expect(playLink).toBeInTheDocument();
+  expect(historyLink).toBeInTheDocument();
+});
