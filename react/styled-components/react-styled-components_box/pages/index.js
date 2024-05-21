ea8162888366f8f6fe272e0537bb928723175a90
Björn <bjoern.n@mailbox.org>
2024-05-21 16:11:08 +0200

    add flex to div

--- react/styled-components/react-styled-components_box/pages/index.js
+++ react/styled-components/react-styled-components_box/pages/index.js
@@ -1,12 +1,18 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
 import BoxWithStyledComponents from "@/components/BoxWithClassName/BoxWithStyledComponents.js";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
 export default function HomePage() {
   return (
-    <div>
+    <Container>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
       <BoxWithStyledComponents $isBlack={false} />
       <BoxWithStyledComponents $isBlack={true} />
-    </div>
+    </Container>
   );
 }
