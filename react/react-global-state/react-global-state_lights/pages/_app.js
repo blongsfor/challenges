0de07c61ab04de171168b14d0f6107c73f01844c
Björn <bjoern.n@mailbox.org>
2024-05-22 13:04:13 +0200

    add allLights maping

--- react/react-global-state/react-global-state_lights/pages/_app.js
+++ react/react-global-state/react-global-state_lights/pages/_app.js
@@ -1,22 +1,23 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
-
-const allLights = [
-  { id: 1, name: "Living Room", isOn: false },
-  { id: 1, name: "Kitchen", isOn: false },
-  { id: 1, name: "Bedroom", isOn: false },
-  { id: 1, name: "Bathroom", isOn: false },
-  { id: 1, name: "Garage", isOn: false },
-  { id: 1, name: "Porch", isOn: false },
-  { id: 1, name: "Garden", isOn: false },
-  { id: 1, name: "Office", isOn: false },
-];
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [allLights, setAllLights] = useState([
+    { id: 1, name: "Living Room", isOn: false },
+    { id: 1, name: "Kitchen", isOn: false },
+    { id: 1, name: "Bedroom", isOn: false },
+    { id: 1, name: "Bathroom", isOn: false },
+    { id: 1, name: "Garage", isOn: false },
+    { id: 1, name: "Porch", isOn: false },
+    { id: 1, name: "Garden", isOn: false },
+    { id: 1, name: "Office", isOn: false },
+  ]);
+
   return (
     <Layout>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} allLights={allLights} />
     </Layout>
   );
 }
