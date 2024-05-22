0237b0c5d6721d54d1f6ac72e36e9c3301158fb2
Björn <bjoern.n@mailbox.org>
2024-05-22 12:48:20 +0200

    add array of allLights

--- react/react-global-state/react-global-state_lights/pages/_app.js
+++ react/react-global-state/react-global-state_lights/pages/_app.js
@@ -1,6 +1,17 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
 
+const allLights = [
+  { id: 1, name: "Living Room", isOn: false },
+  { id: 1, name: "Kitchen", isOn: false },
+  { id: 1, name: "Bedroom", isOn: false },
+  { id: 1, name: "Bathroom", isOn: false },
+  { id: 1, name: "Garage", isOn: false },
+  { id: 1, name: "Porch", isOn: false },
+  { id: 1, name: "Garden", isOn: false },
+  { id: 1, name: "Office", isOn: false },
+];
+
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
