992fc4709837cc3ec2af25d7a403de2b24b67640
Björn <bjoern.n@mailbox.org>
2024-05-24 11:16:37 +0200

    input testing component part one

--- react/component-testing/react-component-testing_scorekeeper/components/Input/Input.test.js
+++ react/component-testing/react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -4,17 +4,17 @@ import Input from ".";
 import "@testing-library/jest-dom/extend-expect";
 
 test("renders a label and an input with the correct attributes", () => {
-  render(<label htmlFor="nameOfGame" />);
-  const nameLabel = screen.getByLabelText("nameOfGame");
-  expect(nameLabel).toBeInTheDocument();
-  const labelElement = screen.getByLabelText("nameOfGame");
-  expect(labelElement).toHaveAttribute("for", "nameOfGame");
-
-  render(<Input />);
-  const placeholderInput = screen.getByPlaceholderText(
-    placeholder,
-    "e.g. Dodelido"
+  render(
+    <Input
+      labelText="Name of game"
+      placeholder="e.g. Dodelido"
+      name="nameOfGame"
+    />
   );
+
+  const nameLabel = screen.getByLabelText("Name of game");
+  expect(nameLabel).toBeInTheDocument();
+  const placeholderInput = screen.getByPlaceholderText("e.g. Dodelido");
   expect(placeholderInput).toBeInTheDocument();
 });
 
