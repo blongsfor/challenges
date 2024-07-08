dd018a5bfd5aa39ea72539903910dc203b8a5962
Björn <bjoern.n@mailbox.org>
2024-07-08 14:40:31 +0200

    good vs evil kata

---
+++ kata/goodvsevil.js
@@ -0,0 +1,71 @@
+// Middle Earth is about to go to war. The forces of good will have many battles with the forces of evil. Different
+// races will certainly be involved. Each race has a certain worth when battling against others. On the side of good we have
+//  the following races, with their associated worth:
+//     Hobbits: 1
+//     Men: 2
+//     Elves: 3
+//     Dwarves: 3
+//     Eagles: 4
+//     Wizards: 10
+// On the side of evil we have:
+//     Orcs: 1
+//     Men: 2
+//     Wargs: 2
+//     Goblins: 2
+//     Uruk Hai: 3
+//     Trolls: 5
+//     Wizards: 10
+// Although weather, location, supplies and valor play a part in any battle, if you add up the worth of the side of good and compare it with
+// the worth of the side of evil, the side with the larger worth will tend to win.
+// // Thus, given the count of each of the races on the side of good, followed by the count of each of the races on the side of evil, determine which side wins.
+// // Input:
+// // The function will be given two parameters. Each parameter will be a string of multiple integers separated by a single space. Each string will
+//  contain the count of each race on the side of good and evil.
+// The first parameter will contain the count of each race on the side of good in the following order:
+//     Hobbits, Men, Elves, Dwarves, Eagles, Wizards.
+// The second parameter will contain the count of each race on the side of evil in the following order:
+//     Orcs, Men, Wargs, Goblins, Uruk Hai, Trolls, Wizards.
+// All values are non-negative integers. The resulting sum of the worth for each side will not exceed the limit of a 32-bit integer.
+// Output:
+// Return "Battle Result: Good triumphs over Evil" if good wins, "Battle Result: Evil eradicates all trace of Good" if evil wins,
+// or "Battle Result: No victor on this battle field" if it ends in a tie.
+
+// go through both teams and see how many of each kind are there and multiply them by theire value
+// compare both numbers with each other (from each team), the side with the higher wort wins
+// output depending on which number is bigger should be then the "Battle Result"
+
+function goodVsEvil(good, evil) {
+  const goodArray = good.split(" ");
+  const evilArray = evil.split(" ");
+
+  let goodMultipliers = [1, 2, 3, 3, 4, 10];
+  let evilmultipliers = [1, 2, 2, 2, 3, 5, 10];
+
+  let goodResult = [];
+  let evilResult = [];
+
+  for (let i = 0; i < goodArray.length; i++) {
+    goodResult[i] = goodArray[i] * goodMultipliers[i];
+  }
+  for (let i = 0; i < evilArray.length; i++) {
+    evilResult[i] = evilArray[i] * evilmultipliers[i];
+  }
+
+  const goodResultNumber = goodResult.reduce(
+    (partialSum, a) => partialSum + a,
+    0
+  );
+  const evilResultNumber = evilResult.reduce(
+    (partialSum, a) => partialSum + a,
+    0
+  );
+
+  if (goodResultNumber > evilResultNumber) {
+    return "Battle Result: Good triumphs over Evil";
+  }
+  if (goodResultNumber < evilResultNumber) {
+    return "Battle Result: Evil eradicates all trace of Good";
+  } else {
+    return "Battle Result: No victor on this battle field";
+  }
+}
